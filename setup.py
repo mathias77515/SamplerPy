d67f5e9cd73ffd28b8e6ee367c155ed7a22c5aa3
mathias77515 <mathias.p.regnier@gmail.com>
2025-07-11 21:32:38 +0200

    update

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import sys
-import setuptools
 import pybind11
+import subprocess
+import sysconfig
 
 class get_pybind_include(object):
     """Helper class to determine the pybind11 include path"""
     def __str__(self):
         return pybind11.get_include()
 
+def brew_prefix(package_name):
+    try:
+        prefix = subprocess.check_output(["brew", "--prefix", package_name], text=True).strip()
+        return prefix
+    except subprocess.CalledProcessError:
+        return None
+
+libomp_prefix = brew_prefix("libomp")
+python_include = sysconfig.get_paths()["include"]
+
 ext_modules = [
     Extension(
         'SamplerPy',                              # name of the module
         ['src/MH.cpp', 'src/utils.cpp'],  # your source files
         include_dirs=[
-            get_pybind_include(),
+            libomp_prefix + '/include',
+            python_include,
             # add other include dirs, e.g. current directory, or where UF23Field.h lives
             '.',
         ],
