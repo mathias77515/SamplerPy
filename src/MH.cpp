546880ac01e7dcf7336e014225576fcd4e4d1ef1
mathias77515 <mathias.p.regnier@gmail.com>
2025-07-18 10:02:56 +0200

    add multiple chains fit

--- src/MH.cpp
+++ src/MH.cpp
@@ -31,7 +31,7 @@ void printProgressBar(int current, int total, int barLength = 100) {
 MetropolisHastings::MetropolisHastings(std::function<double(std::vector<double>)> func)
     : func(func), engine(std::random_device{}()), normal(0.0, 1.0), uniform(0.0, 1.0) 
     {   
-        int numThreads = 1;//omp_get_max_threads();
+        int numThreads = omp_get_max_threads();
         omp_set_num_threads(numThreads);
         std::cout << "\nNumber of available CPU cores: " << numThreads << "\n"<< std::endl;
         stats.accepted = 0.0;
